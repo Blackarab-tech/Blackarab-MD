293a3f0e88ffad85cd9189945685aa6562fc505f
Blackarab-tech <m15108486@gmail.com>
2025-01-12 03:00:25 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "SA1UEapL#OJbym7y_h7YbmMyeo1zmT99sxneXfCmzbT7dqYgk_5A",  //මෙතනට ඔයාගෙ සෙශන් කෝඩ් එක දාන්න
+SESSION_ID: process.env.SESSION_ID || "qElWkaDJ#U_rxdQBvBHxKm39JtnHqWyKxXjs9WCge4uenwmW9rTo",  //මෙතනට ඔයාගෙ සෙශන් කෝඩ් එක දාන්න
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.imgur.com/dHilafW.jpeg",
 ALIVE_MSG: process.env.ALIVE_IMG || "*𝐇𝐞𝐲 𝐡𝐮𝐦𝐚𝐧* ${pushname}\n *𝐈 𝐚𝐦 𝐛𝐥𝐚𝐜𝐤𝐚𝐫𝐚𝐛-𝐦𝐝 𝐚𝐦 𝐚𝐥𝐢𝐯𝐞 𝐧𝐨𝐰 𝐡𝐨𝐰 𝐜𝐚𝐧 𝐢 𝐡𝐞𝐥𝐩 𝐲𝐨𝐮....! 🖐🏻*",
 SUDO_NB: process.env.SUDO_NB || "254712267225",
